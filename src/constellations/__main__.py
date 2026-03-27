8e20ff0837cb2f55b2fbc09aaa8799a304f342db
OpenJ92 <jacob.vartuli.92@gmail.com>
2026-03-27 00:38:14 -0400

    integrate lsystems-core

--- src/constellations/__main__.py
+++ src/constellations/__main__.py
@@ -12,6 +12,18 @@ from typeclass.interpret.run import run, evaluate
 from typeclass.typeclasses.symbols import fmap, pure, ap, compose, many, then, skip, otherwise
 from typeclass.runtime.core import curry
 
+from lsystems.sentences.string import String
+from lsystems.sentences.tuple import Tuple
+from lsystems.productions.static import Static
+from lsystems.productions.stochastic import Stochastic
+from lsystems.productions.precedence import Precedence
+from lsystems.productions.context import ContextSensitive, VariationalContextSensitive
+from lsystems.productions.generation import Generation
+from lsystems.productions.productions import Productions
+from lsystems.lsystem import LSystem
+from lsystems.generate import Generate
+
+
 widths  = Stream |pure| None |fmap| (lambda _: random())
 heights = Stream |pure| None |fmap| (lambda _: random()*2*pi)
 
@@ -26,8 +38,8 @@ class Translate(Automorphism):
         return -1*self.vector
 
 
-vectors = Stream |pure| curry(lambda x, y: array((x, y))) |ap| widths |ap| heights |fmap| Translate
-wectors = Stream |pure| curry(lambda x, y: array((x, y))) |ap| widths |ap| heights |fmap| Translate
+vectors  = Stream |pure| curry(lambda x, y: array((x, y))) |ap| widths |ap| heights |fmap| Translate
+wectors  = Stream |pure| curry(lambda x, y: array((x, y))) |ap| widths |ap| heights |fmap| Translate
 composed = Stream |pure| curry(lambda x, y: evaluate(x |compose| y)) |ap| vectors |ap| wectors
 
 def delay(f):
@@ -42,26 +54,48 @@ def token(c):
 def tree_parser():
     tree = None
 
-    def delayed_tree():
+    def _tree():
         return tree
 
     lbrack = token("[")
     rbrack = token("]")
 
     leaf   = lbrack |then| rbrack
-    branch = lbrack |then| (Parser |many| delay(delayed_tree)) |skip| rbrack
+    branch = lbrack |then| (Parser |many| delay(_tree)) |skip| rbrack
 
     leaf   = leaf   |fmap| (lambda _: Tree(None, Sequence([])))
     branch = branch |fmap| (lambda xs: Tree(None, Sequence(xs)))
-
     tree = leaf |otherwise| branch
+
     return tree
 
 parser = evaluate(tree_parser())
-results = parser.run("[[[[[[]][]][[[][]][]]C][[[][[]]][[][]]]][[[][]]\
-                      [[[[]][]][[][[][A]]]]][][[[[][]][[[]][]]][[[][[\
-                      ][]]][]]][[][[[][]][[B[]][[][]]]]][[[[[]]][]][[\
-                      ][Q[][[[]][]]]]]]")
 
+def extract(shape, st):
+    children = shape.children
+    n = length(children)
+    st_children = take(n, st.children.force())
+
+    return Tree(
+        st.value,
+        zip_with(extract, children, st_children)
+    )
+
+# Start symbol
+sentence = String("X")
+
+# Productions (deterministic)
+productions = Productions(String)
+productions.add("X", Static(String("F+[[X]-X]-F[-FX]+X")))
+productions.add("F", Static(String("FF")))
+
+# Alphabet includes every symbol we might emit
+alphabet = set("FX+-[]")
+
+lsys = LSystem(alphabet, productions, sentence)
+
+gen = Generate(lsys, depth=6)
+result = gen.run()
+
+results = parser.run(result)
 tree = evaluate(results[0][0] |fmap| (lambda _: random()))
-
