35fd848de89cf1a7b8b670d112471cac5622ff67
OpenJ92 <jacob.vartuli.92@gmail.com>
2026-04-24 15:42:48 -0400

    offset computation into Applicative/Fold op

--- src/constellations/compositions/signal_bloom/utils.py
+++ src/constellations/compositions/signal_bloom/utils.py
@@ -9,6 +9,7 @@ from typeclass.runtime.core import curry
 from functools import lru_cache
 from hashlib import sha256
 from numpy import array, pi, exp, cos, sin
+from numpy.linalg import norm
 from numpy.random import default_rng
 
 def length(sequence):
@@ -24,65 +25,18 @@ def extract(shape, st):
         Sequence(tuple(zipwith(extract, children, st_children)))
     )
 
-def path_random(seed=0):
-    def f(p):
-        h = sha256((str(p) + str(seed)).encode()).digest()
-        rng = default_rng(int.from_bytes(h[:8], "little"))
-        return rng.random()
-    return f
+def collect_leaves(tree):
+    if not tree.children._values:
+        return [tree.value]
+    out = []
+    for child in tree.children._values:
+        out.extend(collect_leaves(child))
+    return out
 
-def ascending_segment_length_tree(alpha: float = 0.7, floor: float = 0.0):
-    r = path_random()
-
-    return paths() |fmap| (
-            lambda p: sum(
-                floor + (1.0 - floor) * r(p[:i]) / (1 + i)**alpha
-                for i in range(1, len(p) + 1)
-            )
-        )
-
-def radius_for_depth( depth, base_radius=1.0, alpha= 0.8,):
-    return base_radius / (1 + depth) ** alpha
-
-
-def angle_for_prefix(prefix, angle_seed=0,):
-    r = path_random(angle_seed)
-    return 2 * pi * r(prefix)
-
-
-def offset_for_prefix(prefix, base_radius=1.0, alpha=0.8, angle_seed=0,):
-    depth = len(prefix) - 1
-    radius = radius_for_depth(depth, base_radius, alpha,)
-    angle = angle_for_prefix(prefix, angle_seed)
-
-    return array(( radius * cos(angle), radius * sin(angle),))
-
-
-def make_offset_from_path(base_radius, alpha=0, angle_seed=3):
-    @lru_cache(maxsize=None)
-    def offset_from_path(p):
-        if len(p) == 0:
-            return array((0.0, 0.0))
-
-        parent = p[:-1]
-        return (offset_from_path(parent) + offset_for_prefix(p, base_radius, alpha, angle_seed,))
-
-    return offset_from_path
-
-
-def make_reader_composition_from_path():
-    @lru_cache(maxsize=None)
-    def reader_composition_from_path(p):
-        if len(p[0]) == 0:
-            return Reader(lambda x: x)
-
-        path, reader = p
-        parent = path[:-1]
-
-
-        return Reader\
-          |pure| curry(lambda r, q: r |compose| q)\
-            |ap| reader_composition_from_path(parent)\
-            |ap| reader
-
-    return reader_composition_from_path
+def classify(leaves, line_sample):
+    lposition, line = line_sample
+    pos, reader = min(
+        leaves,
+        key=lambda leaf: norm(lposition - leaf[0]),
+    )
+    return reader, line
