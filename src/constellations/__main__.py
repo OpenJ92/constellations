10d3f1e6009922477a1bdcb0bd5683de4655807a
OpenJ92 <jacob.vartuli.92@gmail.com>
2026-05-14 17:35:21 -0400

    clean-up main

--- src/constellations/__main__.py
+++ src/constellations/__main__.py
@@ -52,28 +52,5 @@
 
 
 ## from constellations.compositions.twisted_obelisk import *
-from numpy import linspace
-import time
-from typeclass.data.sequence import Sequence
 
 from constellations.compositions.signal_bloom import *
-from constellations.geometry.core import SegmentStrip
-from constellations.interpreters.svg import SVG
-
-samples = SegmentStrip(linspace(0, 1.5, 150))
-machine_samples = machine_samples                                         \
-        |fmap| (lambda morphism: samples |fmap| morphism)                 \
-        |fmap| (lambda segment: segment |fmap| Matrix(array([[1,0,0]
-                                                            ,[0,0,1]]).T))
-
-compiled = evaluate(machine_samples)
-print("compile:", time.time())
-
-computation = evaluate(take(500, compiled))
-print("compute:", time.time())
-
-SVG().write_to_file(
-    "src/constellations/compositions/signal_bloom/renders/svg/78.svg"
-    , computation
-    )
-print("write:", time.time())
